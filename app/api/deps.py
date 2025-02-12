3db37c73173d122a9e3c1efd018a567568c37e35
신현승 <tlsgustm12@gmail.com>
2025-02-12 23:02:54 +0900

    .

--- app/api/deps.py
+++
@@ -1,9 +0,0 @@
-from typing import Generator
-from fastapi import Depends, HTTPException, status
-
-def get_db() -> Generator:
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close() 
